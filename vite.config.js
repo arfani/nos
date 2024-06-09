6cac1515e0e10d5931ce948b7c5b3169650a73df
arfani <arfanihidayat@gmail.com>
2024-06-09 23:30:48 +0800

    update vite config to manifest some files

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/client/home.css',
+                'resources/css/client/detail-product.css',
+                'resources/js/client/countdown.js',
+                'resources/js/client/detail-product.js',
+                'resources/js/client/home.js',
             ],
             refresh: true,
         }),
