a95eda62dda75aca8daa988f74dc8ac3cc7b67b4
arfani <arfanihidayat@gmail.com>
2025-08-02 23:17:00 +0800

    fix mobile view - list dikeranjang ga full terlihat

--- resources/views/layouts/client/cart.blade.php
+++ resources/views/layouts/client/cart.blade.php
@@ -25,7 +25,8 @@
     {{-- END ALERT --}}
     <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
         <div class="indicator">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
             </svg>
@@ -33,15 +34,20 @@
                 x-text="$store.cart.totalItem"></span>
         </div>
     </div>
-    <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content min-w-96 bg-base-100 shadow !border-2 !border-primary">
+    <div tabindex="0"
+        class="mt-3 z-[1] card card-compact dropdown-content min-w-80 bg-base-100 shadow !border-2 !border-primary">
         <div class="card-body">
             <span class="font-bold text-lg" x-text="$store.cart.totalItem + ' total item'"></span>
             <div class="list-product max-h-80 overflow-auto flex flex-col gap-2">
                 <template x-for="item in $store.cart.items">
-                    <div class="cart-item" :key="item.product.id">
+                    <div class="cart-item mb-3" :key="item.product.id">
                         <div class="flex gap-2">
-                            <img :src="item.product.product_pictures.length ? `/storage/${item.product.product_pictures[0].path}` : ''" alt="product-image" width="60px" class="rounded">
-                            
+                            <div class="image">
+                                <img :src="item.product.product_pictures.length ?
+                                    `/storage/${item.product.product_pictures[0].path}` :
+                                    ''"
+                                    alt="product-image" width="150" height="150" class="rounded">
+                            </div>
                             <div class="name flex-1">
                                 <div x-text="item.product.name"></div>
                                 <template x-if="item.product_variant">
